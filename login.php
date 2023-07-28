0a57a0f8f35ac41749c2710b827d400fed32e29d
Indra Meyers <67623845+DarK-Zer0@users.noreply.github.com>
2023-07-28 09:25:00 -0400

    Created php files

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Propex</title>
+        <meta charset="utf-8">
+        <link rel="icon" href="./img/icon.jpg">
+        <link rel="stylesheet" href="./styles.css">
+        <script src="./scripts.js"></script>
+    </head>
+    <body>
+    </body>
+</html>
