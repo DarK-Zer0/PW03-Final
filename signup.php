2012cb8025de5e44e83e04a866953a2c6ab25d8e
Marcus1634 <freem.marcus@gmail.com>
2023-07-28 17:22:49 -0400

    Reformatting
    The Register box has been shifted down slightly, now the Terms of Use and Privacy Policy popups completely cover it when they are clicked. Also added appropriate generic text to Terms of Use and Privacy Policy.

--- signup.php
+++ signup.php
@@ -46,14 +46,60 @@
 			<!-- Terms of Use Popup -->
 			<div class="popup popup-hidden" id="popup-terms">
 			  <h2>Terms of Use</h2>
-			  <p>Terms of Use Content</p>
+			  <p>Terms of Use for Propex
+
+Welcome to Propex! These terms and conditions govern your use of our website and services. By accessing or using our platform, you agree to be bound by these terms.
+<br><br>
+1. Account Registration: You must create an account. Keep your login credentials confidential.
+<br><br>
+2. User Content: Any content you submit on our platform is your responsibility. Do not upload inappropriate or copyrighted material.
+<br><br>
+3. Prohibited Activities: You must not engage in any illegal, harmful, or abusive activities on our website.
+<br><br>
+4. Intellectual Property: Our website and its content are protected by copyright and other laws. Do not use our content without permission.
+<br><br>
+5. Disclaimer: We strive for accuracy, but we do not guarantee the completeness or accuracy of information on our platform.
+<br><br>
+6. Limitation of Liability: Propex is not liable for any direct or indirect damages arising from your use of our website.
+<br><br>
+7. Privacy: Our Privacy Policy governs how we collect and use your data. By using our platform, you consent to our data practices.
+<br><br>
+8. Modifications: We may update these terms from time to time. Check back regularly for any changes.
+<br><br>
+By using Propex, you agree to these Terms of Use. If you do not agree, please refrain from using our platform.
+</p>
 			  <button class="close-button" data-popup="terms" onclick="hidePopup('terms')">Close</button>
 			</div>
 			
 			<!-- Privacy Policy Popup -->
 			<div class="popup popup-hidden" id="popup-privacy">
 			  <h2>Privacy Policy</h2>
-			  <p>Privacy Policy Content</p>
+			  <p>Privacy Policy for Propex
+
+At Propex, we value your privacy and are committed to protecting your personal information. This Privacy Policy explains how we collect, use, and safeguard your data.
+<br><br>
+1. Information Collection: We may collect personal data when you create an account, use our services, or interact with our platform.
+<br><br>
+2. Use of Information: We use your data to provide and improve our services, personalize your experience, and communicate with you.
+<br><br>
+3. Data Sharing: We do not sell your personal information to third parties. We may share data with trusted partners for service delivery.
+<br><br>
+4. Security Measures: We employ industry-standard security measures to protect your data from unauthorized access.
+<br><br>
+5. Cookies: We use cookies to enhance your browsing experience and analyze website traffic.
+<br><br>
+6. Third-Party Links: Our platform may contain links to third-party websites. We are not responsible for their privacy practices.
+<br><br>
+7. Children's Privacy: Our services are not intended for children under 13. We do not knowingly collect data from minors.
+<br><br>
+8. Data Retention: We retain your information as long as necessary for the purposes outlined in this policy.
+<br><br>
+9. Your Rights: You have the right to access, modify, or delete your data. Contact us to exercise these rights.
+<br><br>
+10. Policy Changes: We may update this policy periodically. Check back to review any revisions.
+<br><br>
+By using Propex, you agree to our Privacy Policy. If you have any questions or concerns, please contact us.
+</p>
 			  <button class="close-button" data-popup="privacy" onclick="hidePopup('privacy')">Close</button>
 			</div>
 			
